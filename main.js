bae2861c6719998343b4b7e47ce5f0df59ed31cc
Vlad <wlad3430@gmail.com>
2021-10-14 21:24:47 +0300

    Доработка

--- main.js
+++ main.js
@@ -1,24 +1,55 @@
-const player1 = {
+// Task #0
+
+const playerOne = {
   name: 'Scorpion',
   hp: 100,
   img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
-  weapon: ['iceStorm'],
+  weapon: ['AK-47'],
   attack: function () {
     return console.log(player1.name + 'Fight...');
   }
 }
 
-const player2 = {
+const playerTwo = {
   name: 'Kitana',
   hp: 100,
   img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
-  weapon: ['iceStorm'],
+  weapon: ['M-16'],
   attack: function () {
     return console.log(player2.name + 'Fight...');
   }
 }
 
 
-function createPlayer() {
-  const player = document
+// Task #1
+
+function createPlayer(player, playerName, lifes) {
+  const player1 = document.createElement('div')
+  const player2 = document.createElement('div')
+  const progressbar = document.createElement('div')
+  const character = document.createElement('div')
+  const life = document.createElement('div')
+  const name = document.createElement('div')
+  const img = document.createElement('img')
+  const arena = document.querySelector('.arenas')
+
+
+  player1.classList.add('player1')
+  player2.classList.add('player2')
+  progressbar.classList.add('progressbar')
+  character.classList.add('character')
+  life.classList.add('life')
+  life.style.width = 100 + '%'
+  name.classList.add('name')
+  name.textContent = playerOne.name
+  img.src = playerOne.img
+
+  arena.appendChild(player1)
+  player1.appendChild(progressbar)
+  player1.appendChild(character)
+  progressbar.appendChild(life)
+  progressbar.appendChild(name)
+  character.appendChild(img)
 }
+createPlayer('player1', 'Scorpion', 90)
+createPlayer('player2', 'Kitana', 70)
