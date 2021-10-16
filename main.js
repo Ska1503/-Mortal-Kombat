97726085d258bd0b8f37947865cfa7018929f19b
Vlad <wlad3430@gmail.com>
2021-10-16 12:17:26 +0300

    Homework2

--- main.js
+++ main.js
@@ -1,55 +1,50 @@
-// Task #0
 
-const playerOne = {
+const Scorpion = {
   name: 'Scorpion',
   hp: 100,
   img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
   weapon: ['AK-47'],
   attack: function () {
-    return console.log(player1.name + 'Fight...');
+    return console.log(Scorpion.name + 'Fight...');
   }
 }
 
-const playerTwo = {
+const Kitana = {
   name: 'Kitana',
-  hp: 100,
+  hp: 90,
   img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
   weapon: ['M-16'],
   attack: function () {
-    return console.log(player2.name + 'Fight...');
+    return console.log(Kitana.name + 'Fight...')
   }
 }
 
 
-// Task #1
-
-function createPlayer(player, playerName, lifes) {
-  const player1 = document.createElement('div')
-  const player2 = document.createElement('div')
+function createPlayer(playerClassName, playerCharacter) {
+  const player = document.createElement('div')
   const progressbar = document.createElement('div')
   const character = document.createElement('div')
   const life = document.createElement('div')
-  const name = document.createElement('div')
+  const namePers = document.createElement('div')
   const img = document.createElement('img')
   const arena = document.querySelector('.arenas')
 
 
-  player1.classList.add('player1')
-  player2.classList.add('player2')
+  player.classList.add(playerClassName)
   progressbar.classList.add('progressbar')
   character.classList.add('character')
   life.classList.add('life')
-  life.style.width = 100 + '%'
-  name.classList.add('name')
-  name.textContent = playerOne.name
-  img.src = playerOne.img
+  life.style.width = playerCharacter.hp +'%'
+  namePers.classList.add('name')
+  namePers.textContent = playerCharacter.name
+  img.src = playerCharacter.img
 
-  arena.appendChild(player1)
-  player1.appendChild(progressbar)
-  player1.appendChild(character)
+  arena.appendChild(player)
+  player.appendChild(progressbar)
+  player.appendChild(character)
   progressbar.appendChild(life)
-  progressbar.appendChild(name)
+  progressbar.appendChild(namePers)
   character.appendChild(img)
 }
-createPlayer('player1', 'Scorpion', 90)
-createPlayer('player2', 'Kitana', 70)
+createPlayer('player1', Scorpion)
+createPlayer('player2', Kitana)
